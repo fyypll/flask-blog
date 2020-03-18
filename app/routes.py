eea6316509e479e8698eb27a5718c45a22801024
fyypll <fyypll@163.com>
2020-03-18 17:00:30 +0800

    登录页面

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+# 导入表单处理方法
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -18,3 +20,17 @@ def index():
         }
     ]
     return render_template('index.html', title='我的', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # 验证表格中的数据格式是否正确
+    if form.validate_on_submit():
+        # 闪现的信息会出现在页面，当然在页面上要设置
+        flash('用户登录的名户名是:{} , 是否记住我:{}'.format(
+            form.username.data, form.remember_me.data))
+        # 重定向至首页
+        return redirect(url_for('index'))
+    # 首次登录/数据格式错误都会是在登录界面
+    return render_template('login.html', title='登录', form=form)
