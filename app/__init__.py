cb436d8da91469ac0eb55f5102c2667bc29f8179
fyypll <fyypll@163.com>
2020-03-18 18:52:13 +0800

    登录注册等验证完成

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # 创建app应用,__name__是python预定义变量，被设置为使用本模块.
 app = Flask(__name__)
@@ -14,5 +15,9 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 # 绑定app和数据库，以便进行操作
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
+# 在login = LoginManager(app)后面加上即可
+login.login_view = 'login'
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
-from datetime import datetime
+# from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-from app import db
+from app import db, login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -17,6 +19,12 @@ class User(db.Model):
     def __repr__(self):
         return '<用户名:{}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
     __tablename__ = 'post'
@@ -28,3 +36,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+# 用户加载
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
