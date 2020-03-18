0bf6e27b39dc62ac1395863a30c21f05400aeb7e
fyypll <fyypll@163.com>
2020-03-18 17:53:10 +0800

    数据库操作01

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 # 导入配置文件
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # 创建app应用,__name__是python预定义变量，被设置为使用本模块.
 app = Flask(__name__)
@@ -8,4 +10,9 @@ app = Flask(__name__)
 # 添加配置信息
 app.config.from_object(Config)
 
-from app import routes
+# 建立数据库关系
+db = SQLAlchemy(app)
+# 绑定app和数据库，以便进行操作
+migrate = Migrate(app, db)
+
+from app import routes, models
