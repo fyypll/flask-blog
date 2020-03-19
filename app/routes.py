c38270388197932752ee7165d39671e723b8e0bb
fyypll <fyypll@163.com>
2020-03-20 02:00:26 +0800

    后台样式使用bootstrap3修改完成

--- app/routes.py
+++ app/routes.py
@@ -10,20 +10,9 @@ from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'duke'}
-    posts = [
-        {
-            'author': {'username': '刘'},
-            'body': '这是模板模块中的循环例子～1'
-
-        },
-        {
-            'author': {'username': '忠强'},
-            'body': '这是模板模块中的循环例子～2'
-        }
-    ]
-    return render_template('index.html', title='我的', user=user, posts=posts)
+    return render_template('index.html', title='我的', user=user)
 
 
 @app.route('/login', methods=['GET', 'POST'])
--- myblog.py
+++ myblog.py
@@ -3,4 +3,4 @@ from app import app
 
 # 防止被引用后执行，只有在当前模块中才可以使用
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='5000')
