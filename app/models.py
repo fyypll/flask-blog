19fb6d22d210784175899e08fd87847c0072e6bf
fyypll <fyypll@163.com>
2020-03-27 17:15:17 +0800

    修改用户表和文章表关系，当用户被管理员删除时，这个用户的文章跟着删除

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ class User(UserMixin, db.Model):
     # back是反向引用,User和Post是一对多的关系，backref是表示在Post中新建一个属性author，关联的是Post中的user_id外键关联的User对象。
     # lazy属性常用的值的含义，select就是访问到属性的时候，就会全部加载该属性的数据;joined则是在对关联的两个表进行join操作
     # 从而获取到所有相关的对象;dynamic则不一样，在访问属性的时候，并没有在内存中加载数据，而是返回一个query对象, 需要执行相应方法才可以获取对象，比如.all()
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic', cascade='all, delete-orphan')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.now)
 
@@ -44,7 +44,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
     body = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
     post_time = db.Column(db.String(20))
 
     def __repr__(self):
